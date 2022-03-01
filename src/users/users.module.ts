bcbdccb4dfcd5f71768a37903d3e253faf04269a
Aitor Sobera <aitorsobera@gmail.com>
2022-03-01 12:15:39 +0100

    login OK con AuthGuard

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersResolver } from './users.resolver';
+import { Module } from '@nestjs/common'
+import { UsersService } from './users.service'
+import { UsersResolver } from './users.resolver'
 
 @Module({
-  providers: [UsersResolver, UsersService]
+  providers: [UsersResolver, UsersService],
+  exports: [UsersService]
 })
 export class UsersModule {}
